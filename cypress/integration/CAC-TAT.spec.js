cced30108f69def4449a69dedd9677256efe9188
Eduardo Moises <eduardo.moisesqa@hotmail.com>
2024-01-23 10:29:54 -0300

    alteracoes

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,13 +1,13 @@
-///<reference types="Cypress/">
+////<reference types="Cypress/">
 
 const { describe } = require("mocha");
 
 describe('Central de atendimento ao cliente TAT', function(){
 
     it ('verifica o titulo da aplicacao', function () {
-        cypress.visit('./src/index.html')
+        cy.visit('../../src/index.html')
 
-        cypress.title().should('be.equal', )
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT' )
 
         
     })
